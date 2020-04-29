5693f5bf7ce185eb7571452080c6310afe3bb8b7
Hiroto Naya <hiroto.naya.78@gmail.com>
2020-04-30 01:09:31 +0900

    add 1

---
+++ 1_two_sum.js
@@ -0,0 +1,13 @@
+const twoSum = function(nums, target) {
+	const ret = [];
+	for (let i = 0; i < nums.length; i++) {
+		for (let j = i + 1; j < nums.length; j++) {
+			if (nums[i] + nums[j] === target) {
+				ret.push(i);
+				ret.push(j);
+			}
+		}
+	}
+	return ret;
+};
+console.log(twoSum([2, 7, 11, 15], 9));
